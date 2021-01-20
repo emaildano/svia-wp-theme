1979405a05278ef5541114a87b388c12c6ff876f
Daniel Olson <emaildano@gmail.com>
2021-01-20 16:29:39 -0500

    First commit

---
+++ functions.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Theme functions and definitions
+ *
+ * @package HelloElementorChild
+ */
+
+/**
+ * Load child theme css and optional scripts
+ *
+ * @return void
+ */
+function svia_enqueue_scripts()
+{
+	wp_enqueue_style(
+		'svia-wp-theme-styles',
+		get_stylesheet_directory_uri() . '/style.css',
+		[],
+		'0.0.0'
+	);
+
+	wp_enqueue_style('normalize-css', 'https://cdn.jsdelivr.net/npm/normalize.css@latest/normalize.css', [], 'latest', true);
+	wp_enqueue_script('svia-wp-theme-scripts', get_theme_file_uri() . '/main.js', array('algolia-cdn-instantsearch'), '0.0.0', true);
+}
+
+add_action('wp_enqueue_scripts', 'svia_enqueue_scripts', 20);
---
+++ main.js
@@ -0,0 +1,12 @@
+// Algolia Facets
+window.addEventListener("load", function (event) {
+  // let input = document.querySelectorAll(".ais-HierarchicalMenu-link");
+  // input.forEach((item) => {
+  //   item.classList.add(
+  //     "elementor-button-link",
+  //     "elementor-button",
+  //     "elementor-size-xs"
+  //   );
+  //   console.log(item);
+  // });
+});
